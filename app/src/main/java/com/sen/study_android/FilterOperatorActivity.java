1aac5827959b011c4f6f4a04c61064d56aabdddd
zpc18-003 <761063381@qq.cpm>
2019-01-16 15:07:50 +0800

    rxjava transfrom study

---
+++ app/src/main/java/com/sen/study_android/FilterOperatorActivity.java
@@ -0,0 +1,276 @@
+package com.sen.study_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.functions.Function;
+import io.reactivex.functions.Predicate;
+import io.reactivex.schedulers.Schedulers;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class FilterOperatorActivity extends AppCompatActivity {
+    private String tag = getClass().getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter_operator);
+    }
+
+    public void debounce(View view) {
+
+        Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> emitter) throws Exception {
+                emitter.onNext("a");
+                Thread.sleep(500);
+
+                emitter.onNext("b");
+                Thread.sleep(1500);
+
+                emitter.onNext("c");
+                Thread.sleep(500);
+
+                emitter.onNext("e");
+                Thread.sleep(1500);
+
+                emitter.onNext("f");
+                Thread.sleep(500);
+
+
+                emitter.onNext("j");
+
+
+            }
+        })
+                .subscribeOn(Schedulers.io())
+                .debounce(1, TimeUnit.SECONDS)
+                .subscribe(s -> Log.i(tag, "debounce-->" + s));
+    }
+
+
+    /**
+     * 过滤操作符
+     *
+     * @param view
+     */
+    public void distinct(View view) {
+
+        Observable.just(1, 2, 3, 3, 4)
+                .distinct()
+                .subscribe(i -> Log.i(tag, "distinct  1-->" + i));
+
+
+        Observable.just(1, 2, 3, 2, 3, 4)
+                .distinct(new Function<Integer, Integer>() {
+                    @Override
+                    public Integer apply(Integer integer) throws Exception {
+                        //过滤条件
+                        return integer % 2;
+                    }
+                })
+                .subscribe(i -> Log.i(tag, "distinct  2->" + i));
+
+    }
+
+
+    /**
+     * 只过了和上一个数据相等的数据
+     *
+     * @param view
+     */
+    public void distinctUntilChange(View view) {
+
+        Observable.just(1, 2, 2, 3, 1, 4, 4)
+                .distinctUntilChanged()
+                .subscribe(i -> Log.i(tag, "distinctUntilChange   ->" + i));
+    }
+
+    /**
+     * 只发射第i个数据
+     *
+     * @param view
+     */
+    public void elementAt(View view) {
+        Observable.just(1, 2, 3, 4, 5)
+                .elementAt(2)
+                .subscribe(i -> Log.i(tag, "elementAt   ->" + i));
+    }
+
+    /**
+     * 按条件过滤数据
+     *
+     * @param view
+     */
+    public void filter(View view) {
+
+        Observable.just(1, 2, 3, 4)
+                .filter(new Predicate<Integer>() {
+                    @Override
+                    public boolean test(Integer integer) throws Exception {
+                        return integer > 2;
+                    }
+                }).subscribe(i -> Log.i(tag, "filter   ->" + i));
+    }
+
+    /**
+     * 只发射第一个数据
+     *
+     * @param view
+     */
+
+    public void first(View view) {
+        Observable.just(1, 2, 3, 4)
+                .first(0)
+                .subscribe(i -> Log.i(tag, "first   ->" + i));
+
+
+    }
+
+
+    /**
+     * 只发射第一个数据,与first的区别：返回值不同
+     *
+     * @param view
+     */
+
+    public void firstElement(View view) {
+        Observable.just(1, 2, 3, 4)
+                .firstElement()
+                .subscribe(i -> Log.i(tag, "firstElement   ->" + i));
+
+
+    }
+
+    /**
+     * 不关心数据发射，当数据发射完成后执行complete
+     *
+     * @param view
+     */
+    public void ignoreElement(View view) {
+        Observable.just(1, 2, 3, 4)
+                .ignoreElements()
+                .subscribe(() -> Log.i(tag, "ignoreElement----->"));
+    }
+
+    public void last(View view) {
+        Observable.just(1, 2, 3, 4)
+                .last(2)
+                .subscribe(i -> Log.i(tag, "last----->" + i));
+
+
+        Observable.just(1, 2, 3, 4)
+                .lastElement()
+                .subscribe(i -> Log.i(tag, "lastElement----->" + i));
+    }
+
+
+    /**
+     * 类型过滤
+     *
+     * @param view
+     */
+    public void ofType(View view) {
+
+        Observable<Float> o = Observable.just(1, 1.2f, 2)
+                .ofType(Float.class);
+
+        o.subscribe(i -> Log.i(tag, "ofType----->" + i));
+
+    }
+
+    public void simple(View view) {
+        Observable<Integer> o = Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                emitter.onNext(1);
+                Thread.sleep(500);
+                emitter.onNext(2);
+                Thread.sleep(500);
+                emitter.onNext(3);
+                Thread.sleep(500);
+                emitter.onNext(4);
+                Thread.sleep(500);
+                emitter.onNext(5);
+                Thread.sleep(500);
+            }
+        }).sample(1, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io());
+
+        o.subscribe(i -> Log.i(tag, "simple----->" + i));
+    }
+
+
+    /**
+     * 跳过n个数据
+     *
+     * @param view
+     */
+    public void skip(View view) {
+
+        Observable.just(1, 2, 3, 4, 5)
+                .skip(2)
+                .subscribe(i -> Log.i(tag, "skip----->" + i));
+
+        Observable.just(1, 2, 3, 4, 5)
+                .skipLast(2)
+                .subscribe(i -> Log.i(tag, "skipLast----->" + i));
+    }
+
+    /**
+     * 只取n个数据
+     *
+     * @param view
+     */
+    public void take(View view) {
+
+        Observable.just(1, 2, 3, 4, 5)
+                .take(2)
+                .subscribe(i -> Log.i(tag, "take----->" + i));
+
+        Observable.just(1, 2, 3, 4, 5)
+                .takeLast(2)
+                .subscribe(i -> Log.i(tag, "takeLast----->" + i));
+    }
+
+    /**
+     * 过滤超时的任务
+     *
+     * @param view
+     */
+
+    public void timeout(View view) {
+
+
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter)     {
+
+
+                try {
+                    Thread.sleep(400);
+                    emitter.onNext(2);
+                    Thread.sleep(800);
+                    emitter.onNext(3);
+                    Thread.sleep(1200);
+                    emitter.onNext(4);
+                    emitter.onComplete();
+                }catch (Exception e){
+
+                }
+
+
+            }
+        })
+                .timeout(1, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                .subscribe(i -> Log.i(tag, "timeout----->" + i),
+                        e->Log.i(tag,"tieout err="+e.getMessage()));
+    }
+}
--- app/src/main/java/com/sen/study_android/rxjava/RxJavaStudyActivity.java
+++ app/src/main/java/com/sen/study_android/rxjava/RxJavaStudyActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import com.sen.study_android.FilterOperatorActivity;
 import com.sen.study_android.R;
 import io.reactivex.*;
 import io.reactivex.functions.Consumer;
@@ -24,7 +25,7 @@ import java.util.concurrent.Callable;
 
 public class RxJavaStudyActivity extends AppCompatActivity {
 
-    private String tag=getClass().getSimpleName();
+    private String tag = getClass().getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +36,7 @@ public class RxJavaStudyActivity extends AppCompatActivity {
 
 
     public void helloRxjava(View view) {
-        Flowable.fromArray(new String[]{"one","two","three"}).subscribe(s->Log.i(tag,"helloRxjava-->"+s));
+        Flowable.fromArray(new String[]{"one", "two", "three"}).subscribe(s -> Log.i(tag, "helloRxjava-->" + s));
 
        /* Flowable.fromArray(new String[]{"one","two","three"}).subscribe(new Consumer<String>() {
             @Override
@@ -47,11 +48,11 @@ public class RxJavaStudyActivity extends AppCompatActivity {
 
     public void createObservables(View view) {
 
-        Observable<String>  one =Observable.fromArray("a","b","c");
+        Observable<String> one = Observable.fromArray("a", "b", "c");
 
-        Observable<String> two=Observable.just("1","2","3");
+        Observable<String> two = Observable.just("1", "2", "3");
 
-        Observable<String> three=Observable.create(new ObservableOnSubscribe<String>() {
+        Observable<String> three = Observable.create(new ObservableOnSubscribe<String>() {
             @Override
             public void subscribe(ObservableEmitter<String> emitter) throws Exception {
 
@@ -71,23 +72,22 @@ public class RxJavaStudyActivity extends AppCompatActivity {
 //        });
 
 
-
-       one.subscribe(s->Log.i(tag,"createObservables one-->"+s));
-       two.subscribe(s->Log.i(tag,"createObservables two-->"+s));
-       three.subscribe(s->Log.i(tag,"createObservables three-->"+s));
+        one.subscribe(s -> Log.i(tag, "createObservables one-->" + s));
+        two.subscribe(s -> Log.i(tag, "createObservables two-->" + s));
+        three.subscribe(s -> Log.i(tag, "createObservables three-->" + s));
 
     }
 
     public void createFlowable(View view) {
 
-        Flowable<String> one=Flowable.fromArray("a","b","c");
-        one.subscribe(s->Log.i(tag,"createFlowable one -->"+s));
+        Flowable<String> one = Flowable.fromArray("a", "b", "c");
+        one.subscribe(s -> Log.i(tag, "createFlowable one -->" + s));
 
-        Flowable<String> two=Flowable.just("1","2","3");
-        two.subscribe(s->Log.i(tag,"createFlowable two-->"+s));
+        Flowable<String> two = Flowable.just("1", "2", "3");
+        two.subscribe(s -> Log.i(tag, "createFlowable two-->" + s));
 
 
-        Flowable<String> three=Flowable.fromPublisher(new Publisher<String>() {
+        Flowable<String> three = Flowable.fromPublisher(new Publisher<String>() {
             @Override
             public void subscribe(Subscriber<? super String> s) {
                 s.onNext("A");
@@ -96,60 +96,59 @@ public class RxJavaStudyActivity extends AppCompatActivity {
 
             }
         });
-        three.subscribe(s->Log.i(tag,"createFlowable three-->"+s));
+        three.subscribe(s -> Log.i(tag, "createFlowable three-->" + s));
 
-        Flowable<String> four=Flowable.fromCallable(new Callable<String>() {
+        Flowable<String> four = Flowable.fromCallable(new Callable<String>() {
             @Override
             public String call() throws Exception {
                 return "call";
             }
         });
 
-        four.subscribe(s->Log.i(tag,"createFlowable four-->"+s));
+        four.subscribe(s -> Log.i(tag, "createFlowable four-->" + s));
 
 
+        Flowable<String> five = Flowable.just("a", "b");
+        five.subscribe(s -> Log.i(tag, "createFlowable five-->" + s));
 
-        Flowable<String> five=Flowable.just("a","b");
-        five.subscribe(s->Log.i(tag,"createFlowable five-->"+s));
 
-
-        Flowable<String> six=Flowable.create(new FlowableOnSubscribe<String>() {
+        Flowable<String> six = Flowable.create(new FlowableOnSubscribe<String>() {
             @Override
             public void subscribe(FlowableEmitter<String> s) throws Exception {
                 s.onNext("A");
                 s.onNext("B");
                 s.onNext("C");
             }
-        },BackpressureStrategy.DROP);
+        }, BackpressureStrategy.DROP);
 
 
-
-        six.subscribe(s->Log.i(tag,"createFlowable six-->"+s));
+        six.subscribe(s -> Log.i(tag, "createFlowable six-->" + s));
 
     }
 
     public void createSingle(View view) {
 
-        Single<String> single=Single.just("single");
-        single.subscribe(s->Log.i(tag,"create single--->"+s));
+        Single<String> single = Single.just("single");
+        single.subscribe(s -> Log.i(tag, "create single--->" + s));
     }
 
 
     /**
      * AsyncSubject:只有当调用onComplete时才会发射最后一个数据，如果发生错误则不会发射数据，仅仅传递错误
+     *
      * @param view
      */
     public void createAsyncSubject(View view) {
-        AsyncSubject asyncSubject=AsyncSubject.create();
+        AsyncSubject asyncSubject = AsyncSubject.create();
         asyncSubject.onNext("a");
         asyncSubject.onNext("b");
 
-        new Thread(){
+        new Thread() {
             @Override
             public void run() {
                 super.run();
                 try {
-                    Thread.sleep( 70);
+                    Thread.sleep(70);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -158,7 +157,7 @@ public class RxJavaStudyActivity extends AppCompatActivity {
             }
         }.start();
 
-        new Thread(){
+        new Thread() {
             @Override
             public void run() {
                 super.run();
@@ -167,23 +166,23 @@ public class RxJavaStudyActivity extends AppCompatActivity {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                asyncSubject.subscribe(s->Log.i(tag,"create AsyncSubject 2--->"+s));
+                asyncSubject.subscribe(s -> Log.i(tag, "create AsyncSubject 2--->" + s));
 
                 try {
-                    Thread.sleep( 50);
+                    Thread.sleep(50);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
 
-                asyncSubject.subscribe(s->Log.i(tag,"create AsyncSubject 3--->"+s));
+                asyncSubject.subscribe(s -> Log.i(tag, "create AsyncSubject 3--->" + s));
 
 
                 try {
-                    Thread.sleep( 150);
+                    Thread.sleep(150);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-                asyncSubject.subscribe(s->Log.i(tag,"create AsyncSubject 4--->"+s));
+                asyncSubject.subscribe(s -> Log.i(tag, "create AsyncSubject 4--->" + s));
             }
         }.start();
 
@@ -192,50 +191,53 @@ public class RxJavaStudyActivity extends AppCompatActivity {
 
     /**
      * BehaviorSubject会缓存最后一个发射的数据，当有订阅新的观察者时，会将最后的发射数据发送给订阅的观察者
+     *
      * @param view
      */
     public void createBehaviorSubject(View view) {
-        BehaviorSubject<String> behaviorSubject=BehaviorSubject.createDefault("default");
+        BehaviorSubject<String> behaviorSubject = BehaviorSubject.createDefault("default");
 
-        behaviorSubject.subscribe(s->Log.i(tag,"createBehaviorSubject 1-->"+s));
+        behaviorSubject.subscribe(s -> Log.i(tag, "createBehaviorSubject 1-->" + s));
         behaviorSubject.onNext("1");
-        behaviorSubject.subscribe(s->Log.i(tag,"createBehaviorSubject 2-->"+s));
+        behaviorSubject.subscribe(s -> Log.i(tag, "createBehaviorSubject 2-->" + s));
         behaviorSubject.onNext("2");
         behaviorSubject.onComplete();
 
-        behaviorSubject.subscribe(s->Log.i(tag,"createBehaviorSubject 3-->"+s));
+        behaviorSubject.subscribe(s -> Log.i(tag, "createBehaviorSubject 3-->" + s));
         behaviorSubject.onNext("3");
     }
 
 
     /**
      * PublishSubject,观察者只能订阅发生订阅时间之后Observable的事件
+     *
      * @param view
      */
     public void createPublishSubject(View view) {
-        PublishSubject<String> publishSubject=PublishSubject.create();
+        PublishSubject<String> publishSubject = PublishSubject.create();
         publishSubject.onNext("a");
-        publishSubject.subscribe(s->Log.i(tag,"createPublishSubject  1-->"+s));
+        publishSubject.subscribe(s -> Log.i(tag, "createPublishSubject  1-->" + s));
         publishSubject.onNext("b");
-        publishSubject.subscribe(s->Log.i(tag,"createPublishSubject  2-->"+s));
+        publishSubject.subscribe(s -> Log.i(tag, "createPublishSubject  2-->" + s));
         publishSubject.onNext("c");
         publishSubject.onComplete();
     }
 
     /**
      * ReplaySubject 会发射所有已发射过的数据给观察者
+     *
      * @param view
      */
 
     public void createReplaySubject(View view) {
 
-        ReplaySubject<String> replaySubject=ReplaySubject.create();
+        ReplaySubject<String> replaySubject = ReplaySubject.create();
         replaySubject.onNext("a");
-        replaySubject.subscribe(s->Log.i(tag,"createReplaySubject  1-->"+s));
+        replaySubject.subscribe(s -> Log.i(tag, "createReplaySubject  1-->" + s));
         replaySubject.onNext("b");
-        replaySubject.subscribe(s->Log.i(tag,"createReplaySubject  2-->"+s));
+        replaySubject.subscribe(s -> Log.i(tag, "createReplaySubject  2-->" + s));
         replaySubject.onNext("c");
-        replaySubject.subscribe(s->Log.i(tag,"createReplaySubject  3-->"+s));
+        replaySubject.subscribe(s -> Log.i(tag, "createReplaySubject  3-->" + s));
         replaySubject.onComplete();
 
     }
@@ -245,16 +247,21 @@ public class RxJavaStudyActivity extends AppCompatActivity {
     }
 
     public void asyncOperators(View view) {
-        startActivity(new Intent(this,AsyncOperatorsActivity.class));
+        startActivity(new Intent(this, AsyncOperatorsActivity.class));
 
     }
 
 
     public void createOperators(View view) {
-        startActivity(new Intent(this,CreateOpratorsActivity.class));
+        startActivity(new Intent(this, CreateOpratorsActivity.class));
     }
 
     public void transformOperators(View view) {
-        startActivity(new Intent(this,TransformOperatorActivity.class));
+        startActivity(new Intent(this, TransformOperatorActivity.class));
+    }
+
+    public void filterOperators(View view) {
+
+        startActivity(new Intent(this, FilterOperatorActivity.class));
     }
 }
